506ddde36cc81e1a203b0364d454c6b777e199ba
Peter Novotnak <peter@cyrusbio.com>
2016-06-03 12:01:38 -0700

    functional tests now run locally

--- test_harnesses/functional_test.sh
+++ test_harnesses/functional_test.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 #
-#  Run a simple functional test
+#   Run a simple functional test. Run this from the package root, after you've
+#  configured a Django site at $1.
 #
 #
 # $1 = Django site name
 # $2 = Port to use
 # $3 = String to search for in headers
 
+trap EXIT
+
 # Start the server
-../$1/manage.py runserver 127.0.0.0:$2 &
+./$1/manage.py runserver 127.0.0.1:$2 &
 SERVER_PID=$!
+trap "kill $SERVER_PID" EXIT
+
+sleep 5
 
 # Run the test
 [[ -n "$(curl -I --silent localhost:$2 | grep $3)" ]] || exit 1
-
-kill $SERVER_PID
